46047f99b73ff08d8e49993bcda2ab11602df478
mono-cry <s.warita.03.11@gmail.com>
2020-07-22 10:22:29 +0900

    add new file

---
+++ PrimeLambda.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int64_t n;
+    cin >> n;
+    
+    auto f = [](int64_t num) -> bool {
+        if (num < 2) 
+            return false;
+        else if (num == 2)
+            return true;
+        else if (num % 2 == 0)
+            return false;
+            
+        for (int i = 3; i <= sqrt(num); i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    };
+    
+    cout << n << " is " << (f(n) ? "Prime number." : "Not prime number.") << endl;
+}
+
