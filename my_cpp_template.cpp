74c80d1b58dab55ecf32cc78c886f8e16d3708f1
mono-cry <s.warita.03.11@gmail.com>
2020-08-07 14:35:21 +0900

    My C++ template

---
+++ my_cpp_template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(nullptr)
+#define rep(i, n) for(int i = 0; i < (int)(n); ++i)
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(),(x).end()
+#define SZ(x) ((int)(x).size())
+
+int main() {
+	FASTIO;
+	
+}
